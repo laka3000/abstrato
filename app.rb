af02767f715f6837629b3319eb1962b730cb2dcd
Alexandre Manikowski <alexandre.karpinski.m@gmail.com>
2020-07-25 16:23:57 -0300

    allow frames

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require 'csv'
 require 'json'
 require_relative 'term_creator'
 
+configure do
+  set :protection, except: [:frame_options]
+end
+
 get '/new_term' do
   TermCreator.new_term.to_json
 end
